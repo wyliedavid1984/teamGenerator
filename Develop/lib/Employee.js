1bcd12c24287b05c48347889c061a88608809ae3
wylie <wyliedavid1984@gmail.com>
2020-12-19 10:04:42 -0500

    added class for employee

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,14 @@
+// TODO: Write code to define and export the Employee class
+class Employee{
+
+    constructor (name, id, email){
+
+        this.name = name;
+        this.id = id;
+        this.email = email
+    }
+
+
+}
+
+module.export = Employee;
