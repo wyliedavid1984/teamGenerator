9b7916b6bff2ef87d620e602928100bc30f80993
wylie <wyliedavid1984@gmail.com>
2020-12-19 11:12:41 -0500

    added methods

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,5 +1,5 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
-const Employee = require("./Employee");
+let Employee = require("./Employee.js");
 
 class Manager extends Employee {
 
@@ -8,5 +8,8 @@ class Manager extends Employee {
         this.office = office;
     }
 }
+
+const m1 = new Manager(manager, "Nate", 1, "Nate@io.com", 100)
+console.log(m1)
 
 module.export = Manager;
